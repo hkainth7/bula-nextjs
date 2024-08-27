550d8c403a438eab71c9d2619003b9d5708886ba
Harjit Kainth <harjit.kainth7@gmail.com>
2024-08-26 21:06:10 -0700

    fontsize

--- app/_components/ContentTypography/index.tsx
+++ app/_components/ContentTypography/index.tsx
@@ -2,7 +2,7 @@ import { styled } from "@mui/material/styles";
 import { Typography } from "@mui/material";
 
 export const ContentTypography = styled(Typography)({
-	fontSize: "14px",
+	fontSize: "16px",
 	textAlign: "center",
 	lineHeight: 1.6,
 	padding: "8px",
