f0bebc9253e103ac2b4df0f0329e993c72a25c56
Harjit Kainth <harjit.kainth7@gmail.com>
2024-08-26 21:03:57 -0700

    make more readable

--- app/_components/ContentTypography/index.tsx
+++ app/_components/ContentTypography/index.tsx
@@ -2,7 +2,6 @@ import { styled } from "@mui/material/styles";
 import { Typography } from "@mui/material";
 
 export const ContentTypography = styled(Typography)({
-	fontWeight: "lighter",
 	fontSize: "14px",
 	textAlign: "center",
 	lineHeight: 1.6,
