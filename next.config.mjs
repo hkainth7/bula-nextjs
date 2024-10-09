649cd460c04f952a3ee9c159586a8da9ba947d5d
Harjit Kainth <harjit.kainth7@gmail.com>
2024-10-09 11:38:50 -0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	swcMinify: true,
+	productionBrowserSourceMaps: false,
+	reactStrictMode: true,
+	async headers() {
+		return [
+			{
+				// Apply these headers to all files under the root (public directory and all other files)
+				source: "/(.*)",
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public, max-age=31536000, immutable",
+					},
+				],
+			},
+		];
+	},
+};
 
 export default nextConfig;
