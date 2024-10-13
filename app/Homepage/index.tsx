ba639ad5c13b52e3195e0a6977b7baebfb8029e5
Harjit Kainth <harjit.kainth7@gmail.com>
2024-10-13 11:56:24 -0700

    update to latest

--- app/Homepage/index.tsx
+++ app/Homepage/index.tsx
@@ -82,6 +82,7 @@ const Homepage = () => {
 									src={data.imgSrc}
 									alt="bula catering services"
 									fill
+									sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
 									quality={100}
 									priority={true}
 									className={styles.backgroundImage}
