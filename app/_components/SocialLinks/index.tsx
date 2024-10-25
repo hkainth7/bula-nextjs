721e0c38353a5447bbaf455c971e9ca671d0023d
Harjit Kainth <harjit.kainth7@gmail.com>
2024-10-24 21:04:11 -0700

    optimize SEO

--- app/_components/SocialLinks/index.tsx
+++ app/_components/SocialLinks/index.tsx
@@ -11,26 +11,30 @@ export const SocialLinks = () => {
 		{
 			href: "https://www.instagram.com/bulahookahlounge?igsh=MTMzengydnk2MXFiNw==",
 			icon: <InstagramIcon fontSize="large" className={styles.icon} />,
+			ariaLabel: "Visit our Instagram page",
 		},
 		{
 			href: "https://youtube.com/@bulahookahlounge?si=-PkGPD7CmQ_NOf4a",
 			icon: <YouTubeIcon fontSize="large" className={styles.icon} />,
+			ariaLabel: "Visit our YouTube page",
 		},
 		{
 			href: "https://youtube.com/@bulahookahlounge?si=-PkGPD7CmQ_NOf4a",
 			icon: <FacebookIcon fontSize="large" className={styles.icon} />,
+			ariaLabel: "Visit our Facebook page",
 		},
 	];
 	return (
 		<Stack direction="row" alignItems="center">
 			{linkData.map((link, index) => (
-				<Link href={link.href} key={index}>
+				<Link href={link.href} key={index} aria-label={link.ariaLabel}>
 					{link.icon}
 				</Link>
 			))}
 			<Link
 				href="https://youtube.com/@bulahookahlounge?si=-PkGPD7CmQ_NOf4a"
 				className={styles.tikTokIcon}
+				aria-label="Visit our TikTok page"
 			>
 				<TikTokIcon color={"#497445"} />
 			</Link>
