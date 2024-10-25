d7b7536ac512837a813b98e3d93f747418cae9fe
Harjit Kainth <harjit.kainth7@gmail.com>
2024-10-24 20:19:52 -0700

    seo optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,8 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-	title: "Hookah Lounge, Relax and Unwind - Bula Hookah Lounge",
+	title:
+		"Best Hookah Lounge in Vancouver & Burnaby - Relax at Bula Hookah Lounge",
 	description:
 		"Discover Bula Hookah Lounge, the best hookah lounge in Vancouver and Burnaby. Relax with premium Shisha and create unforgettable moments in an inviting atmosphere. Happiness is a puff away!",
 	icons: {
